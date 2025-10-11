e30613fb51172a0635cfea174e5d22d5961ba1c1
denniskurilov <denniskurilov@users.noreply.github.com>
2025-10-11 13:53:36 -0700

    update SQL script

--- sql/main.sql
+++ sql/main.sql
@@ -8,6 +8,7 @@ CREATE TABLE APP.EMPLOYEE (
     FIRST_NAME      VARCHAR2(30 CHAR),
     LAST_NAME       VARCHAR2(30 CHAR),
     AGE             NUMBER,
+    IS_ACTIVE       NUMBER(1),
     EMAIL           VARCHAR2(50 CHAR)
 );
 
@@ -48,16 +49,17 @@ INSERT INTO EMPLOYEE (
     FIRST_NAME,
     LAST_NAME,
     AGE,
+    IS_ACTIVE,
     EMAIL
 )
-SELECT 'William', 'Smith',    38, 'william.smith@gmail.com'     FROM dual UNION ALL
-SELECT 'Jack',    'Jones',    23, 'jack.jones@gmail.com'        FROM dual UNION ALL
-SELECT 'George',  'Williams', 19, 'george.williams@hotmail.com' FROM dual UNION ALL
-SELECT 'Thomas',  'Brown',    65, 'thomas.brown@gmail.com'      FROM dual UNION ALL
-SELECT 'James',   'Taylor',   52, 'james.taylor@gmail.com'      FROM dual UNION ALL
-SELECT 'Michael', 'Johnson',  41, 'michael.johnson@yahoo.com'   FROM dual UNION ALL
-SELECT 'David',   'Miller',   29, 'david.miller@gmail.com'      FROM dual UNION ALL
-SELECT 'Robert',  'Davis',    47, 'robert.davis@outlook.com'    FROM dual UNION ALL
-SELECT 'Daniel',  'Wilson',   33, 'daniel.wilson@gmail.com'     FROM dual UNION ALL
-SELECT 'Andrew',  'Clark',    56, 'andrew.clark@gmail.com'      FROM dual;
+SELECT 'William', 'Smith',    38, 1, 'william.smith@gmail.com'     FROM dual UNION ALL
+SELECT 'Jack',    'Jones',    23, 1, 'jack.jones@gmail.com'        FROM dual UNION ALL
+SELECT 'George',  'Williams', 19, 1, 'george.williams@hotmail.com' FROM dual UNION ALL
+SELECT 'Thomas',  'Brown',    65, 1, 'thomas.brown@gmail.com'      FROM dual UNION ALL
+SELECT 'James',   'Taylor',   52, 1, 'james.taylor@gmail.com'      FROM dual UNION ALL
+SELECT 'Michael', 'Johnson',  41, 0, 'michael.johnson@yahoo.com'   FROM dual UNION ALL
+SELECT 'David',   'Miller',   29, 1, 'david.miller@gmail.com'      FROM dual UNION ALL
+SELECT 'Robert',  'Davis',    47, 1, 'robert.davis@outlook.com'    FROM dual UNION ALL
+SELECT 'Daniel',  'Wilson',   33, 0, 'daniel.wilson@gmail.com'     FROM dual UNION ALL
+SELECT 'Andrew',  'Clark',    56, 1, 'andrew.clark@gmail.com'      FROM dual;
 COMMIT;
